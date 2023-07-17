388961e7801683c40b6fa44abf6a466c563479c1
Hussein Ahmed <elgeniusabdelsanea@gmail.com>
2023-07-17 17:33:20 +0300

    testing

--- dist/server.dev.js
+++ dist/server.dev.js
@@ -1,10 +1,10 @@
 "use strict";
 
-var http = requir('http');
+var http = require('http');
 
 var app = require("./app/app");
 
-var port = process.env.port || 8080;
-var server = http.createserver(app);
+var port = process.env.port || 8800;
+var server = http.createServer(app);
 server.listen(port, console.log("server is running on port $(PORT)"));
 //# sourceMappingURL=server.dev.js.map
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const http = requir('http');
+const http = require('http');
 const app = require("./app/app");
-const port = process.env.port || 8080;
-const server = http.createserver(app);
+const port = process.env.port || 8800;
+const server = http.createServer(app);
 server.listen(port, console.log(`server is running on port $(PORT)`));
