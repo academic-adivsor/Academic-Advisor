787bb33ef0276b1ffa0449ee54ea9566a433769a
Hussein Ahmed <elgeniusabdelsanea@gmail.com>
2023-07-30 17:30:08 +0300

    Admin

--- model/staff/Admin.js
+++ model/staff/Admin.js
@@ -16,34 +16,6 @@ const adminSchema = new mongoose.Schema({
         type: String,
         default: "admin",
     },
-    academicTerms: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "AcademicTerm",
-    }, ],
-    programs: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Program",
-    }, ],
-    yearGroups: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "YearGroup",
-    }, ],
-    academicYears: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "AcademicYear",
-    }, ],
-    classLevels: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "ClassLevel",
-    }, ],
-    teachers: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Teacher",
-    }, ],
-    students: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Student",
-    }, ],
 }, {
     timestamps: true,
 });
--- model/staff/dist/Admin.dev.js
+++ model/staff/dist/Admin.dev.js
@@ -18,35 +18,7 @@ var adminSchema = new mongoose.Schema({
   role: {
     type: String,
     "default": "admin"
-  },
-  academicTerms: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "AcademicTerm"
-  }],
-  programs: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Program"
-  }],
-  yearGroups: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "YearGroup"
-  }],
-  academicYears: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "AcademicYear"
-  }],
-  classLevels: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "ClassLevel"
-  }],
-  teachers: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Teacher"
-  }],
-  students: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Student"
-  }]
+  }
 }, {
   timestamps: true
 }); //model
