b4240527db1733614aa1439f9fa0c2f62f37a8ce
Hussein Ahmed <elgeniusabdelsanea@gmail.com>
2023-09-30 04:25:57 +0300

    test

--- dist/server.dev.js
+++ dist/server.dev.js
@@ -10,5 +10,5 @@ var app = require("./app/app");
 
 var port = process.env.port || 8800;
 var server = http.createServer(app);
-server.listen(port, console.log("server is running on port $(PORT)")); //gg
+server.listen(port, console.log("server is running on port $(PORT)"));
 //# sourceMappingURL=server.dev.js.map
--- server.js
+++ server.js
@@ -5,4 +5,3 @@ const app = require("./app/app");
 const port = process.env.port || 8800;
 const server = http.createServer(app);
 server.listen(port, console.log(`server is running on port $(PORT)`));
-//gg
