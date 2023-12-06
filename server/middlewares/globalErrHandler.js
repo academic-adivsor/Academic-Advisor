dbf0b762021a03e34fe48c4727da243c7b2d67c0
Hussein Ahmed <elgeniusabdelsanea@gmail.com>
2023-12-06 05:56:43 +0200

    globalErrHandler

--- server/middlewares/globalErrHandler.js
+++ server/middlewares/globalErrHandler.js
@@ -1,20 +1,22 @@
 const globalErrHandler = (err, req, res, next) => {
-    const stack = err.stack;
-    const message = err.message;
-    const status = err.status ? err.status : "Failed";
-    const statuscode = err.statuscode ? err.statuscode : 500;
-    res.status(statuscode).json({
-        status,
-        message,
-        stack,
-    });
+  //status
+  //message
+  //stack
+  const stack = err.stack;
+  const message = err.message;
+  const status = err.status ? err.status : "failed";
+  const statusCode = err.statusCode ? err.statusCode : 500;
+  res.status(statusCode).json({
+    status,
+    message,
+    stack,
+  });
 };
 
-// not found
+//Not found
 const notFoundErr = (req, res, next) => {
-    const err = new Error(`Can't find ${req.originalUrl} on server`);
-    err.statusCode = 404; // Set the status code to 404
-    next(err);
+  const err = new Error(`Can't find ${req.originalUrl} on the server`);
+  next(err);
 };
 
 module.exports = { globalErrHandler, notFoundErr };
