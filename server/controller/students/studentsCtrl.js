eabd8ca0a6a881651b910b99c62d2c5368df3c3b
budderrar <ahmednasser82380@gmail.com>
2023-12-24 21:17:31 +0200

    hussein

--- server/controller/students/studentsCtrl.js
+++ server/controller/students/studentsCtrl.js
@@ -1,7 +1,7 @@
 const AysncHandler = require("express-async-handler");
-const Exam = require("../../model/Academic/Exam");
-const ExamResult = require("../../model/Academic/ExamResults");
-const Student = require("../../model/Academic/Student");
+const Exam = require("../../model/academic/Exam");
+const ExamResult = require("../../model/academic/ExamResults");
+const Student = require("../../model/qcademic/Student");
 const Admin = require("../../model/staff/Admin");
 const generateToken = require("../../utlis/generateToken");
 const { hashPassword, isPassMatched } = require("../../utlis/helpers");
